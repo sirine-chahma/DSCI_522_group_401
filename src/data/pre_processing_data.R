47dc6fc3dfc4799833195f628ea95357e41f33e3
sirine_chahma <sirine.chahma@supelec.fr>
2020-01-22 17:49:17 -0800

    create the pre_processsing script that splits the data between train and test data

--- src/data/pre_processing_data.R
+++ src/data/pre_processing_data.R
@@ -1,16 +1,42 @@
 # author: Karanpal	Singh, Sreejith	Munthikodu, Sirine	Chahma
 # date: 2020-01-17
 #
-" This script cleans the data that can be found in the input path. It does all the 
-pre-processing work, it splits the data between a train set and a test set.
-It saves the two sets in the data folder in the project directory.
+" This script splits the data given as an input into training andd test data.
+It saves the two sets in two seperate csv files, in the file that is given as an input.
 This script takes two arguments : a path/filename pointing to the data to be read in
 and a path/filename pointing to where the cleaned data should live. 
 
-Usage: pre_processing_data.R --row_data_loc=<row_data_loc> --clean_data_loc=<clean_data_loc>
+
+Usage: pre_processing_data.R --input_file=<input_file> --output_dir=<out_dir>
+
+Options:
+--input_file=<input_file>             Path (including filename) to raw data (csv file)
+--output_dir=<output_dir>   Path to directory where the processed data should be written
 " -> doc
 
 library(tidyverse)
 library(testthat)
 library(docopt)
+set.seed(123)
 
+opt <- docopt(doc)
+main <- function(input_file, output_dir){
+
+  # read data
+  data <- read_csv(input_file)  
+  
+  # split into training and test data sets
+  sample_size <- floor(0.80 * nrow(data))
+
+  train_index <- sample(seq_len(nrow(data)), size = sample_size)
+
+  train_data <- data[train_index, ]
+  test_data <- data[-train_index, ]
+
+  
+  # write training and test data to csv files
+  write_csv(train_data, paste0(output_dir, "/medical_cost_data_train.csv"))
+  write_csv(test_data, paste0(output_dir, "/medical_cost_data_test.csv"))
+}
+
+main(opt[["--input_file"]], opt[["--output_dir"]])
